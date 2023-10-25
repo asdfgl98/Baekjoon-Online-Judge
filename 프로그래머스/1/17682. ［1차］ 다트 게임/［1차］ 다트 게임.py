8eb30e8a3852c285f41e9abb431eeb98678fff20
JIHUN <83624652+asdfgl98@users.noreply.github.com>
2023-10-25 12:27:06 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,30 @@
+def solution(dartResult):
+    answer = 0
+    result =[]
+    
+    rule = {
+        "S": 1,
+        "D": 2,
+        "T": 3
+    }
+    
+    score = ''
+    
+    for i in dartResult:
+        if i.isdigit():
+            score += i
+        elif i in rule:
+            result.append(int(score)**rule[i])
+            score = ''
+        elif i == '#':
+            result[-1] *= -1
+        elif i == '*':
+            result[-1] *= 2
+            
+            if len(result) >= 2:
+                result[-2] *= 2
+        
+                
+            
+    
+    return sum(result)
